4bc4bb9393d8703a0a96923e41d9f0db38335978
shabbirrajpoot786786-tech <shabbirrajpoot786786@gmail.com>
2025-11-18 19:24:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let firstname=prompt("Enter your first name")
+console.log("Hello "+firstname)
+
+let lastname=prompt("enter your last name")
+console.log("Hello "+lastname)
+
+let fullname=prompt("enter your fullname")
+console.log("Hello"+fullname)
+
+let age=prompt("enter your age")
+console.log(`hello ${age}`)
+
+let citu=prompt("enter your city")
+console.log(`hello ${city}`)
+
+let country=prompt("enter your country")
+console.log(`hello ${country}`)
+
+let schoolname=prompt("enter your schoolname")
+console.log(`hello ${schoolname}`)
+
+let favoritesubject=prompt("enter your favoritesubject")
+console.log(`hello ${favoritesubject}`)
+
+let yearofbirth=prompt("enter your yearofbirth")
+console.log(`hello ${yearofbirth}`)
+
+let currentyear=prompt("enter your currentyear")
+console.log(`hello ${currentyear}`)
+
+
+
+
+
+
+
+
+
